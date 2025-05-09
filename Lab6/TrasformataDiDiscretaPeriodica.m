367839339b1c60e12de145a4b18de421e84470fe
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2025-05-09 09:23:53 +0200

    Lab 7 e 6
    fatti lab 7 e 6

---
+++ Lab6/TrasformataDiDiscretaPeriodica.m
@@ -0,0 +1,32 @@
+clear all, clf;
+T = 1e-3 % s
+N = 100
+N1 = 3
+A = 2 % V
+
+t = (0:N-1)*T
+x = A*(t <= N1*T)
+
+subplot(2,2,1)
+stem(t,x)
+xlabel('t[s]')
+ylabel('x(t) [V]')
+
+Fp = 1/T
+F = Fp/N
+f = (0:N-1)*F
+X = T * fft(x)
+subplot(2,2,2)
+plot(f, real(X), 'r', f, imag(X), 'm')
+xlabel('f[Hz]')
+ylabel('X(f) [Vs]')
+legend('Re X', 'Im X')
+
+df = 1 % Hz
+fa = 0:df:Fp
+Xa = A*T*(exp(-i*2*pi*fa*T*(N1+1))-1) ./ (exp(-i*2*pi*fa*T)-1) %./ fa la divisione punto a punto
+subplot(2,2,4)
+plot(fa, real(Xa), 'r', fa, imag(Xa), 'm')
+xlabel('f[Hz]')
+ylabel('X(f) [Vs]')
+legend('Re X', 'Im X')
